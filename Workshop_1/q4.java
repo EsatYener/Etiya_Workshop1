dc51d580eaf72b067ee9a104a2b0c457d66771b0
Esat Yener <92437764+EsatYener@users.noreply.github.com>
2024-04-22 09:07:43 +0300

    Update q4.java

--- Workshop_1/q4.java
+++ Workshop_1/q4.java
@@ -1,7 +1,7 @@
-//kendisi hariç pozitif tam bölenlerinin toplamı kendisine eşitse mükemmel sayıdır.
+
 public class q4 {
     public static void main(String[] args) {
-        int sayi = 28;  // Kontrol etmek istediğiniz sayıyı buraya yazabilirsiniz
+        int sayi = 28;  
         if (isPerfectNumber(sayi)) {
             System.out.println(sayi + " bir mükemmel sayıdır.");
         } else {
@@ -10,20 +10,18 @@ public class q4 {
     }
 
     // Mükemmel sayıyı kontrol eden fonksiyon
-    public static boolean isPerfectNumber(int num) { //burada kullanıcıya bir return yaptığımız fonksiyon tipi ,
-        //return edilecek değişken tipiyle aynı olmalıdır.Fonksiyon eğer hiçbir şey return etmiyorsa
-        // fonksiyon tipi void olmalıdır.
+    public static boolean isPerfectNumber(int num) { 
         if (num < 1) {
             return false;
         }
         int toplam = 0;
-        // Sayının kendisi hariç pozitif bölenlerini bul ve topla
-        for (int i = 1; i <= num / 2; i++) { // num/2 koşulu tam bölenlerinin toplamından kendisini hariç tutar.Bunun yerine i<=num deyip en son toplam - num yapılabilir.ama sağlıksız.
+        
+        for (int i = 1; i <= num / 2; i++) { 
             if (num % i == 0) {
                 toplam += i;
             }
         }
-        // Bölenlerin toplamı sayıya eşitse, mükemmel sayıdır
-        return toplam == num; // return den sonra kontrol edilmesi gereken bir şey yazılabilir.
+        
+        return toplam == num; 
     }
 }
